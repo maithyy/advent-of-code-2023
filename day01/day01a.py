d35929896a9bb38fee15450330ea968a1195f133
maithyy <maithyle265@yahoo.com>
2023-11-30 23:34:23 -0800

    Added cleaner version

--- day01/day01a.py
+++ day01/day01a.py
@@ -10,3 +10,24 @@ def main():
 
 if __name__ == "__main__":
     print(main())
+
+# Cleaner version
+#
+# def getFirstValue(line):
+#     for i in range(len(line)):
+#         if line[i].isdigit():
+#             return str(line[i])
+            
+# def getLastValue(line):
+#     for i in range(len(line)-1, -1, -1):
+#         if line[i].isdigit():
+#             return str(line[i])
+
+# def main():
+#     result = 0
+#     with open('input.txt') as f:
+#         for line in f:
+#             firstValue = getFirstValue(line)
+#             lastValue = getLastValue(line)
+#             result += int(firstValue + lastValue)
+#     return result
