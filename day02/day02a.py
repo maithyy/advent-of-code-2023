88309277e284d31d9c6b25eebc51ac34d9a843d4
maithyy <maithyle265@yahoo.com>
2023-12-01 23:59:42 -0800

    Added day 2

---
+++ day02/day02a.py
@@ -0,0 +1,26 @@
+def possible_game(cube_sets):
+    for s in cube_sets:
+        s = s.split(",")
+        for pair in s:
+            amt, color = pair.split()
+            amt = int(amt)
+            if color in possible and possible[color] < amt:
+                return False
+    return True
+
+
+possible = {"red": 12, "green": 13, "blue": 14}
+def main():
+    result = 0
+    with open('input.txt') as f:
+        for line in f:
+            game_id, cube_sets = line.strip().split(': ')
+            game_id = int(game_id.split()[1])
+            cube_sets = cube_sets.split('; ')
+            if possible_game(cube_sets):
+                result += game_id
+        
+    return result
+
+if __name__ == "__main__":
+    print(main())
---
+++ day02/day02b.py
@@ -0,0 +1,30 @@
+def get_fewest(cube_sets):
+    result_dict = {}
+    for s in cube_sets:
+        s = s.split(",")
+        for pair in s:
+            amt, color = pair.split()
+            amt = int(amt)
+            if color in result_dict:
+                result_dict[color] = max(result_dict[color], amt)
+            else:
+                result_dict[color] = amt
+    return result_dict
+
+
+possible = {"red": 12, "green": 13, "blue": 14}
+def main():
+    result = 0
+    with open('input.txt') as f:
+        for line in f:
+            _, cube_sets = line.strip().split(': ')
+            cube_sets = cube_sets.split('; ')
+            power = 1
+            for val in get_fewest(cube_sets).values():
+                power *= val
+            result += power
+        
+    return result
+
+if __name__ == "__main__":
+    print(main())
